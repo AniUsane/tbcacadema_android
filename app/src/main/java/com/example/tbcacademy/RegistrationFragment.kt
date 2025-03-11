01bbdd40b553e51d846afb5c814964aaf1e8571d
Ani <a.usanetashvili1@gmail.com>
2025-03-11 13:53:27 +0400

    Created new project

---
+++ app/src/main/java/com/example/tbcacademy/RegistrationFragment.kt
@@ -0,0 +1,10 @@
+package com.example.tbcacademy
+
+import com.example.tbcacademy.databinding.FragmentRegistrationBinding
+
+class RegistrationFragment : BaseFragment<FragmentRegistrationBinding>(FragmentRegistrationBinding::inflate) {
+    override fun start() {
+
+    }
+
+}
